8359485ab715fef48f98bdef0a857246383175a7
multix20 <multix2009>
2025-02-19 20:15:08 -0300

    Terminando UserContex

--- src/utils/AppProvider.jsx
+++ src/utils/AppProvider.jsx
@@ -1,14 +1,12 @@
-import CursosProvider from '../context/CursosContext'
-import UserProvider from '../context/UserContext'
+import { UserProvider } from '../context/UserContext';
+import CursosProvider from '../context/CursosContext';
 
-export default function AppProvider ({ children }) {
+export default function AppProvider({ children }) {
   return (
-
     <UserProvider>
       <CursosProvider>
         {children}
       </CursosProvider>
     </UserProvider>
-
-  )
+  );
 }
