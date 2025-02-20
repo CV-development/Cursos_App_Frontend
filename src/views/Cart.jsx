4bc89ebf396368a94f5fbc587fbbda37862dae72
CV-development <carlos.valderas.a@gmail.com>
2025-02-20 00:01:43 -0300

    Se pueden agregar objetos al carro

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -1,11 +1,33 @@
+import { useContext } from 'react'
+import { CartContext } from '../context/CartContext'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 
-export default function Cart () {
+export default function Cart() {
+  const { cart, deleteFromCart } = useContext(CartContext)
+
+  const handleDelete = (id_curso, id_usuario) => {
+    deleteFromCart(id_curso, id_usuario)
+  }
+
   return (
     <div>
       <NavBar />
-      <h1>CARRO</h1>
+      <h1>Carro de Compras</h1>
+      {cart.map((userCart) => (
+        <div key={userCart.id_usuario}>
+          <h2>Usuario: {userCart.id_usuario}</h2>
+          <ul>
+            {userCart.cursos.map((curso) => (
+              <li key={curso.id_curso}>
+                {curso.nombre} - ${curso.precio}
+                <button onClick={() => handleDelete(curso.id_curso, userCart.id_usuario)}>Eliminar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      <button onClick={() => alert('Pagar')}>Pagar</button>
       <Footer />
     </div>
   )
