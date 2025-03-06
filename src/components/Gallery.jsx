7a7b9a2235c7bff13309da5396d6f02bc3a3da87
CV-development <carlos.valderas.a@gmail.com>
2025-03-06 01:35:01 -0300

    Actualización de componentes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
 import { Row } from 'react-bootstrap'
 import CursoCard from './CursoCard'
+import api from '../services/api'
 
-export default function Gallery({ cursos }) {
+export default function Gallery() {
+  const [cursos, setCursos] = useState([])
+
+  useEffect(() => {
+    const fetchCursos = async () => {
+      try {
+        const res = await api.get('/api/cursos')
+        setCursos(res.data)
+      } catch (error) {
+        console.error('Error al obtener los cursos:', error)
+      }
+    }
+
+    fetchCursos()
+  }, [])
+
   return (
     <div>
       <Row>
